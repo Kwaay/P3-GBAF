3c6407f74989f128e602cfaf8b12b0d3d575022a
Kway <benoit.dum74@gmail.com>
2021-01-12 21:23:10 +0100

    Début PHP

--- connexion.php
+++ connexion.php
@@ -8,7 +8,25 @@
         
     <body>
     <?php include ("header.php") ?>
-    
+    <?php
+    require('config.php');
+        session_start();
+        if (isset($_POST['username'])){
+            $username = stripslashes($_REQUEST['username']);
+            $username = mysqli_real_escape_string($conn, $username);
+            $password = stripslashes($_REQUEST['password']);
+            $password = mysqli_real_escape_string($conn, $password);
+                $query = "SELECT * FROM `users` WHERE username='$username' and password='".hash('sha256', $password)."'";
+            $result = mysqli_query($conn,$query) or die(mysql_error());
+            $rows = mysqli_num_rows($result);
+            if($rows==1){
+                $_SESSION['username'] = $username;
+                header("Location: index.php");
+            }else{
+                $message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
+        }
+        }
+    ?>
 
         <div class="connexion_form">
             <h3><u>Connexion à l'Extranet</u></h3>
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
     <head>
         <div class="header">
              <a href="index.php"><img src="images/logo_gbaf_p3.png" height=125 width=125 /></a>  
+             <h1>Bienvenue <?php echo $_SESSION['username']; ?>
         </div>
     </head>
 </html>
