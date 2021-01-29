d5df2e7e204c7e3ecd48f510349580cdd1d6e12d
Kwaay <benoit.dum74@gmail.com>
2021-01-29 10:04:45 +0100

    Ajout de la page des mentions légales

--- mentions_legales.php
+++ mentions_legales.php
@@ -8,6 +8,67 @@
         
     <body>
     <?php include ("header.php"); ?>
+        <div class="text-mentionslegales">
+            <p>
+                <h2><u> Mentions légales | GBAF </u></h2>
+                <br />
+                <h3><u>Identité</u></h3>
+                Nom du site web : GBAF 
+                <br />
+                Adresse : localhost 
+                <br />
+                Propriétaire : X 
+                <br />
+                Responsable de publication : X 
+                <br />
+                <br />
+                Conception et réalisation : X 
+                <br />
+                Animation : X 
+                <br />
+                Hébergement : X 
+                <br />
+
+                <h3><u>Conditions d’utilisation</u></h3>
+                L’utilisation du présent site implique l’acceptation pleine et entière des conditions générales d’utilisation décrites ci-après. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment.
+
+                <h3><u>Informations</u></h3>
+                Les informations et documents du site sont présentés à titre indicatif, ne revêtent pas un caractère exhaustif, et ne peuvent engager la responsabilité du propriétaire du site.
+
+                Le propriétaire du site ne peut être tenu responsable des dommages directs et indirects consécutifs à l’accès au site.
+
+                <h3><u>Interactivité</u></h3>
+                Les utilisateurs du site peuvent éventuellement y déposer du contenu, apparaissant sur le site dans des espaces dédiés (notamment via les commentaires). Le contenu déposé reste sous la responsabilité de leurs auteurs, qui en assument pleinement l’entière responsabilité juridique.
+
+                Le propriétaire du site se réserve le droit de retirer sans préavis et sans justification tout contenu déposé par les utilisateurs qui ne satisferait pas à la charte déontologique du site ou à la législation en vigueur.
+
+                <h3><u>Propriété intellectuelle</u></h3>
+                Sauf mention contraire, tous les éléments accessibles sur le site (textes, images, graphismes, logo, icônes, sons, logiciels, etc.) restent la propriété exclusive de leurs auteurs, en ce qui concerne les droits de propriété intellectuelle ou les droits d’usage. 
+
+                Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de l’auteur.
+
+                Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient est considérée comme constitutive d’une contrefaçon et peut être poursuivie en justice. 
+
+                Les marques et logos reproduits sur le site sont déposés par les sociétés qui en sont propriétaires.
+
+                <h3><u>Liens</u><h3>
+                <h4><u>Liens sortants</u></h4>
+                Le propriétaire du site décline toute responsabilité et n’est pas engagé par le référencement via des liens hypertextes, de ressources tierces présentes sur le réseau Internet, tant en ce qui concerne leur contenu que leur pertinence.
+
+                <h4><u>Liens entrants</u></h4>
+                Le propriétaire du site autorise les liens hypertextes vers l’une des pages de ce site, à condition que ceux-ci ouvrent une nouvelle fenêtre et soient présentés de manière non équivoque afin d’éviter :
+
+                tout risque de confusion entre le site citant et le propriétaire du site
+                ainsi que toute présentation tendancieuse, ou contraire aux lois en vigueur.
+                Le propriétaire du site se réserve le droit de demander la suppression d’un lien s’il estime que le site source ne respecte pas les règles ainsi définies.
+
+                <h3><u>Confidentialité</u></h3>
+                Tout utilisateur dispose d’un droit d’accès, de rectification et d’opposition aux données personnelles le concernant, en effectuant sa demande écrite et signée, accompagnée d’une preuve d’identité.
+
+                Le site ne recueille pas d’informations personnelles, et n’est pas assujetti à déclaration à la CNIL.
+            </p>
+        </div>
+
     <?php include ("footer.php") ?>
     </body>
 </html>
